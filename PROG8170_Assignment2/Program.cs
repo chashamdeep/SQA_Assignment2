0b30a01d1de03f6e0f8b633ebb8ef8c093915a62
chashamdeep kaur <ckaur8336@conestogac.on.ca>
2020-02-19 20:19:33 -0500

    program working

--- PROG8170_Assignment2/Program.cs
+++ PROG8170_Assignment2/Program.cs
@@ -8,43 +8,94 @@ namespace PROG8170_Assignment2
 {
     class Program
     {
-        static int userChoice;
-        static string inputString;
+        static int choice;
+        static string input, validDimensions, output;
+        static int dimensionOne, dimensionTwo, dimensionThree;
         static void Main(string[] args)
         {
             try
             {
                 do
                 {
-                    Console.WriteLine("\n*******************\n");
                     Console.WriteLine("\n 1.Enter triangle Dimensions");
                     Console.WriteLine("\n 2.Exit");
-                    Console.Write("\n Enter your choice; ");
-                    userChoice = GetValue();
-                } while (userChoice != 2);
+                    choice = CheckValue();
+                    MenuAction(choice);
+                } while (choice != 2);
 
             }
-            catch (FormatException e1)
+            catch(Exception e)
             {
-                Console.WriteLine("\n Format Exception: Given input was Invalid");
+                Console.WriteLine("\n Invalid Choice");
             }
-            catch (Exception e2)
+        }
+        public static void MenuAction(int choice)
+        {
+            switch (choice)
             {
-                Console.WriteLine("\n Exception Arise: Segmentation falut/Runtime problem");
+                case 1:
+                    Console.Write("\nEnter triangle Dimension 1: ");
+                    dimensionOne = CheckValue();
+                    Console.Write("\nEnter triangle Dimension 2: ");
+                    dimensionTwo = CheckValue();
+                    Console.Write("\nEnter triangle Dimension 3: ");
+                    dimensionThree = CheckValue();
+                    //check for valid dimensions
+                    bool valid = ValidateDimensions(dimensionOne, dimensionTwo, dimensionThree);
+                    if (valid)
+                    {
+                        validDimensions = "These dimensions could form a triangle\n";
+                        //check triangle type 
+                        output = TriangleSolver.Analyze(dimensionOne, dimensionTwo, dimensionThree);
+                        Console.Write("\nResult : {0}", validDimensions);
+                        Console.Write("\nResult : {0}", output);
+                    }
+                    if(!valid)
+                    {
+                        validDimensions = "These dimensions could not form a triangle\n";
+                        Console.Write("\nResult : {0}", validDimensions);
+                    }
+                    break;
+                case 2:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("\nInput can be either 1 or 2");
+                    break;
             }
-
         }
 
-        // get and check user input value;
-        public static int GetValue()
+        //check user input value;
+        public static int CheckValue()
         {
             int value;
             do
             {
-                Console.Write("Please a +ve integer number only : ");
-                inputString = Console.ReadLine();
-            } while (!int.TryParse(inputString, out value) || value < 0);
+                input = Console.ReadLine();
+                //print if menu options are not valid
+                if (!int.TryParse(input, out value) || value < 0)
+                {
+                    Console.Write("Enter positive integer only\n");
+                }
+            } while (!int.TryParse(input, out value) || value < 0);
             return value;
         }
+        //check dimensions for triangle
+        public static bool ValidateDimensions(int dimensionOne, int dimensionTwo, int dimensionThree)
+        {
+            if(dimensionOne == 0 || dimensionTwo == 0 || dimensionThree == 0)
+            {
+                return false;
+            }
+            else if (dimensionOne + dimensionTwo <= dimensionThree || dimensionOne + dimensionThree <= dimensionTwo || dimensionTwo + dimensionThree <= dimensionOne || (dimensionOne == dimensionTwo && dimensionOne == dimensionThree))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
--- PROG8170_Assignment2/TriangleSolver.cs
+++ PROG8170_Assignment2/TriangleSolver.cs
@@ -8,5 +8,22 @@ namespace PROG8170_Assignment2
 {
     public static class TriangleSolver
     {
+        public static string Analyze(int dimensionOne, int dimensionTwo, int dimensionThree)
+        {
+            string output = "";
+            if (dimensionOne == dimensionTwo && dimensionTwo == dimensionThree)
+            {
+                output = "The triangle is Equilateral\n";
+            }
+            else if (dimensionOne == dimensionTwo || dimensionOne == dimensionThree || dimensionTwo == dimensionThree)
+            {
+                output = "The triangle is Isosceles\n";
+            }
+            else
+            {
+                output = "The triangle is Scalene\n";
+            }
+            return output;
+        }
     }
 }
