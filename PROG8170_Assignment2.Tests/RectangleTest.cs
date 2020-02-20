19719b5958be81dab17840e3d4ee53f9340b717b
chashamdeep kaur <ckaur8336@conestogac.on.ca>
2020-02-20 16:38:31 -0500

    test cases completed

--- PROG8170_Assignment2.Tests/RectangleTest.cs
+++ PROG8170_Assignment2.Tests/RectangleTest.cs
@@ -1,14 +1,65 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace PROG8170_Assignment2.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class RectangleTest
-    {
-        [TestMethod]
-        public void TestMethod1()
+    {       
+
+        [Test]
+        public void Test_Analyze_Input_20_20_20_Expected_Ouptut_Equilateral_Triangle()
         {
+            string result = TriangleSolver.Analyze(20, 20, 20);
+            Assert.AreEqual("The triangle is Equilateral\n", result);
+        }
+
+        [Test]
+        public void Test_Analyze_Input_5_5_5_Expected_Ouptut_Equilateral_Triangle()
+        {
+            string result = TriangleSolver.Analyze(5, 5, 5);
+            Assert.AreEqual("The triangle is Equilateral\n", result);
+        }
+
+        [Test]
+        public void Test_Analyze_Input_20_20_30_Expected_Ouptut_Isocsceles_Triangle()
+        {
+            string result = TriangleSolver.Analyze(20, 20, 30);
+            Assert.AreEqual("The triangle is Isosceles\n", result);
+        }
+
+        [Test]
+        public void Test_Analyze_Input_3_30_30_Expected_Ouptut_Isocsceles_Triangle()
+        {
+            string result = TriangleSolver.Analyze(3, 30, 30);
+            Assert.AreEqual("The triangle is Isosceles\n", result);
+        }
+
+        [Test]
+        public void Test_Analyze_Input_4_8_4_Expected_Ouptut_Isocsceles_Triangle()
+        {
+            string result = TriangleSolver.Analyze(4, 8, 4);
+            Assert.AreEqual("The triangle is Isosceles\n", result);
+        }
+
+        [Test]
+        public void Test_Analyze_Input_17_15_19_Expected_Ouptut_Scalene_Triangle()
+        {
+            string result = TriangleSolver.Analyze(17, 15, 19);
+            Assert.AreEqual("The triangle is Scalene\n", result);
+        }
+
+        [Test]
+        public void Test_Analyze_Input_100_95_86_Expected_Ouptut_Scalene_Triangle()
+        {
+            string result = TriangleSolver.Analyze(100, 95, 86);
+            Assert.AreEqual("The triangle is Scalene\n", result);
+        }
+        [Test]
+        public void Test_Analyze_Input_8_1_12_Expected_Ouptut_Scalene_Triangle()
+        {
+            string result = TriangleSolver.Analyze(8, 1, 12);
+            Assert.AreEqual("The triangle is Scalene\n", result);
         }
     }
 }
